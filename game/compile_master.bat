e5a917f2fcbf459cd564bc25c23f44575108b3c9
AlchemicRaker <natsogames@yahoo.com>
2021-11-26 19:54:13 -0600

    Changes to build for Alchemy65

--- game/compile_master.bat
+++ game/compile_master.bat
@@ -23,19 +23,19 @@ IF DEFINED audio (
 IF DEFINED code (
 	REM -g adds debug information, but the end result .nes file is not
 	REM affected, so leave it in all the time.
-	cc65 -g -Oirs -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 %name%.c --add-source
-	ca65 -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 crt0.s
+	cc65 -g -Oirs -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 %name%.c --add-source -o  BUILD/%name%.s
+	ca65 -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 BUILD/crt0.s
 	ca65 %name%.s -g
 
 	REM -dbgfile does not impact the resulting .nes file.
-	ld65 -C nrom_32k_vert.cfg --dbgfile %name%.dbg -o %name%.nes crt0.o %name%.o nes.lib -Ln labels.txt
+	ld65 -C nrom_32k_vert.cfg --dbgfile %name%.dbg -o %name%.nes BUILD.crt0.o BUILD/%name%.o nes.lib -Ln labels.txt
 
 	del *.o
 
 	mkdir BUILD\
 	move /Y %name%.dbg BUILD\ 
 	move /Y labels.txt BUILD\ 
-	move /Y %name%.s BUILD\ 
+	@REM move /Y %name%.s BUILD\ 
 	move /Y %name%.nes BUILD\ 
 )
 
