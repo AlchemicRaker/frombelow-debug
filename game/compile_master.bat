593b7f94a4ae37c776f7b0b63a9c5bdc17a2481a
AlchemicRaker <natsogames@yahoo.com>
2021-11-26 21:50:15 -0600

    Fix the compile_master.bat to use the BUILD folder

--- game/compile_master.bat
+++ game/compile_master.bat
@@ -23,20 +23,21 @@ IF DEFINED audio (
 IF DEFINED code (
 	REM -g adds debug information, but the end result .nes file is not
 	REM affected, so leave it in all the time.
-	cc65 -g -Oirs -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 %name%.c --add-source -o  BUILD/%name%.s
-	ca65 -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 BUILD/crt0.s
-	ca65 %name%.s -g
+	mkdir BUILD\
+
+	cc65 -g -Oirs -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 %name%.c --add-source -o BUILD/%name%.s
+	ca65 -D VS_SYS_ENABLED=0 -D VS_SRAM_ENABLED=0 crt0.s -o BUILD/crt0.o
+	ca65 BUILD/%name%.s -g -o BUILD/%name%.o
 
 	REM -dbgfile does not impact the resulting .nes file.
-	ld65 -C nrom_32k_vert.cfg --dbgfile %name%.dbg -o %name%.nes BUILD.crt0.o BUILD/%name%.o nes.lib -Ln labels.txt
+	ld65 -C nrom_32k_vert.cfg --dbgfile BUILD/%name%.dbg -o BUILD/%name%.nes BUILD/crt0.o BUILD/%name%.o nes.lib -Ln BUILD/labels.txt
 
-	del *.o
+	del BUILD\*.o
 
-	mkdir BUILD\
-	move /Y %name%.dbg BUILD\ 
-	move /Y labels.txt BUILD\ 
+	@REM move /Y %name%.dbg BUILD\ 
+	@REM move /Y labels.txt BUILD\ 
 	@REM move /Y %name%.s BUILD\ 
-	move /Y %name%.nes BUILD\ 
+	@REM move /Y %name%.nes BUILD\ 
 )
 
 if DEFINED run (
